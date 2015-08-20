4dd819142610381f95d91f627afb9cf78b246f4c
amirahaile <amira.dhaile@gmail.com>
2015-08-20 11:05:26 -0700

    Debugging for Heroku deploy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,55 +2,57 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #
+  # # shippers
+  # USPS = ActiveShipping::USPS.new(:login => ENV["ACTIVESHIPPING_USPS_LOGIN"])
+  #
+  # # TODO: Add :shipper
+  def prepare
+    # response = response.body
+    # merchant = response["merchant"]
+    # buyer = response["buyer"]
+    # parcels = response["packages"] # hash with number keys
 
-  # shippers
-  USPS = ActiveShipping::USPS.new(:login => ENV['ACTIVESHIPPING_USPS_LOGIN'])
-
-  def prepare(merchant, buyer, parcels)
     origin = ActiveShipping::Location.new(
       :country => 'US',
-      :state => merchant[:state],
-      :city => merchant[:city],
-      :zip => merchant[:zip]
+      # :state => merchant["state"],
+      # :city => merchant["city"],
+      # :zip => merchant["zip"]
     )
 
     destination = ActiveShipping::Location.new(
       :country => 'US',
-      :state => buyer[:shipping_state],
-      :city => buyer[:shipping_city],
-      :zip => buyer[:shipping_zip]
+      # :state => buyer[:shipping_state],
+      # :city => buyer[:shipping_city],
+      # :zip => buyer[:shipping_zip]
     )
 
     packages = []
-    our_record = []
-    # parcels is an array of Products from the OrderItem objects
-    parcels.each do |parcel|
-      # to make the request
-      package = ActiveShipping::Package.new(
-        parcel.weight,
-        [parcel.width, parcel.height, parcel.length],
-        :units => :imperial
-      )
-
-      packages << package
-    end
+    # parcels.each do |number, parcel|
+    #   package = ActiveShipping::Package.new(
+    #     parcel["weight"],
+    #     [parcel["width"], parcel["height"], parcel["length"]],
+    #     :units => :imperial
+    #   )
+    #
+    #   packages << package
+    # end
 
     # for our records
-    request = Request.new(origin, destination)
-    parcel.each do |parcel|
-      request.packages << parcel
-    end
+    # Request.new(origin: origin.to_s, destination: destination.to_s)
 
-    redirect_to request_path(origin, destination, packages)
+    request_info
   end
 
-  def request(origin, destination, packages)
-    # make the HTTP request
+  def request_info
+  #   # Error: 'undefined method `content_mime_type' for nil:NilClass'
+  #   # make the HTTP request
+    render :index
   end
-
-  def parse(request)
-    # parse the response
-    # return parsed response
-    # render json: blahblah
-  end
+  #
+  # def parse
+  #   # parse the response
+  #   # return parsed response
+  #   # render json: blahblah
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'application#prepare'
 
-  get '/request' => 'application#request', as: 'request'
-  get '/parse'   => 'application#parse',   as: 'parse'
+  # get '/request' => 'application#request', as: 'request'
+  # get '/parse'   => 'application#parse',   as: 'parse'
 end
